b7629a41d6f809561795d24cb46c1d620992926f
Belal-hossainn <belalhossain.rn@gmail.com>
2022-01-11 21:50:42 +0600

    create, update & delete user method setup

--- Api/index.js
+++ Api/index.js
@@ -3,17 +3,19 @@ const app = express()
 const port = 3000;
 
 const dotenv = require('dotenv');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log( 'Database Connected' ))
 .catch(err => console.log( err ));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- Api/models/User.js
+++ Api/models/User.js
@@ -20,6 +20,7 @@ const UserSchema = new mongoose.Schema(
             type: String,
             default: ''
         }
+        
     }, {timestamps: true}
 );
 
